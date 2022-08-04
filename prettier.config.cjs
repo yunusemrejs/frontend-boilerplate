3e0ff64ecbe81ee3ce0566fa71985b3e2168acc0
llcawc <hostmastername@gmail.com>
2022-08-04 22:38:31 +0300

    update to 2.8.0

---
+++ prettier.config.cjs
@@ -0,0 +1,23 @@
+// prettier.config.cjs
+// https://prettier.io/docs/en/configuration.html
+
+module.exports = {
+  // These settings are duplicated in .editorconfig:
+  tabWidth: 2, // indent_size = 2
+  useTabs: false, // indent_style = space
+  endOfLine: 'lf', // end_of_line = lf
+  semi: false, // default: true
+  singleQuote: true, // default: false
+  jsxSingleQuote: true, // default: false
+  printWidth: 120, // default: 80
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  overrides: [
+    {
+      files: ['*.js', '*.cjs', '*.mjs'],
+      options: {
+        parser: 'flow',
+      },
+    },
+  ],
+}
